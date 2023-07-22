0bab230e3ce4230fd08d0676c895e7a347d6a481
Zewen Xu <zevin9427@gmail.com>
2023-07-22 20:49:49 +0800

    c13 done

--- booktool/c13/c13.asm
+++ booktool/c13/c13.asm
@@ -4,18 +4,29 @@
          ;创建日期：2011-10-30 15:19   
          
 ;===============================================================================
+;用户程序必须符合规定，才能被内核识别和加载，所有操作系统的可执行文件都包含文件的头部
+
+
 SECTION header vstart=0
 
          program_length   dd program_end          ;程序总长度#0x00
          
          head_len         dd header_end           ;程序头部的长度#0x04
 
+        ;内核不要求用户程序提供堆栈空间，而改为由内核动态分配，减轻用户程序编写的负担
+        ;当内核分配堆栈空间之后，就会把堆栈段选择子填写到这个地方，用户程序在这里取的选择子来初始化自己的堆栈
+
          stack_seg        dd 0                    ;用于接收堆栈段选择子#0x08
+         ;用户程序编写者推荐的堆栈大小，1是4kb为单位，4kb的堆栈空间，2是8kb的空间,以此类推
          stack_len        dd 1                    ;程序建议的堆栈大小#0x0c
                                                   ;以4KB为单位
-                                                  
+
+         ;程序的入口，偏移地址，可以通过这个进入到指定的地点                                         
          prgentry         dd start                ;程序入口#0x10 
+         ;程序代码段的起始汇编地址，内核完成加载用户程序和重定向之后，把这个段的选择子填写到这里
+
          code_seg         dd section.code.start   ;代码段位置#0x14
+
          code_len         dd code_end             ;代码段长度#0x18
 
          data_seg         dd section.data.start   ;数据段位置#0x1c
@@ -23,11 +34,21 @@ SECTION header vstart=0
              
 ;-------------------------------------------------------------------------------
          ;符号地址检索表
-         salt_items       dd (header_end-salt)/256 ;#0x24
+         ;要使用操作系统的api如果使用call的话就需要相应的地址，不行，所以我们使用符号名
+         ;使用符号名不会同时公布一个段地址和偏移地址，因为他也不能保证地址不会变化，在操作系统的手册中就是会列出所有的符号名字
+         ;这个符号名就是c语言中的库函数名
+
+         salt_items       dd (header_end-salt)/256 ;#0x24   ,这个用来计算符号名的数量
          
+         ;内核要求，必须要在0x28的位置构造一个表格，在表格在中列出所有要用到的符号名,每个符号名256字节长度，不足用0填充
+         ;用户程序加载的使用内核会分析这个表格，把这里的每个符号名替换成相应的内存地址，这个就是过程的重定位
+
+         ;符号地址检索表
          salt:                                     ;#0x28
+            ;每个标号使用@表示接口
+            ;用户程序只会把自己用到的列出来
          PrintString      db  '@PrintString'
-                     times 256-($-PrintString) db 0
+                     times 256-($-PrintString) db 0   ;每个条目256字节，用于存储符号名
                      
          TerminateProgram db  '@TerminateProgram'
                      times 256-($-TerminateProgram) db 0
@@ -54,19 +75,20 @@ data_end:
 ;===============================================================================
 SECTION code vstart=0
 start:
-         mov eax,ds
-         mov fs,eax
+         mov eax,ds     ;ds指向的是头部
+         mov fs,eax     ;现在让fs指向头部，ds就解放了
      
-         mov eax,[stack_seg]
-         mov ss,eax
-         mov esp,0
+         mov eax,[stack_seg]  ;栈段选择子
+         mov ss,eax           ;
+         mov esp,0            ;并且将esp指向0
      
-         mov eax,[data_seg]
-         mov ds,eax
+         mov eax,[data_seg]   
+         mov ds,eax           ;ds指向自己的数据段
      
          mov ebx,message_1
-         call far [fs:PrintString]
+         call far [fs:PrintString]  ;调用内核过程来打印字符串
      
+         ;调用内核过程来读取磁盘
          mov eax,100                         ;逻辑扇区号100
          mov ebx,buffer                      ;缓冲区偏移地址
          call far [fs:ReadDiskData]          ;段间调用
@@ -76,7 +98,7 @@ start:
      
          mov ebx,buffer 
          call far [fs:PrintString]           ;too.
-     
+         ;调用内核过程返回到内核态
          jmp far [fs:TerminateProgram]       ;将控制权返回到系统 
       
 code_end:
--- booktool/c13/c13_core.asm
+++ booktool/c13/c13_core.asm
@@ -2,8 +2,10 @@
          ;文件名：c13_core.asm
          ;文件说明：保护模式微型核心程序 
          ;创建日期：2011-10-26 12:11
+       
+       ;内核文件还包含了一个头部，记录了各个段的汇编位置，这些数据用来告诉初始化代码怎么加载内核
 
-         ;以下常量定义部分。内核的大部分内容都应当固定 
+         ;以下常量定义部分。内核的大部分内容都应当固定 ,声明一些常数,equ是一个伪指令，不会占内存
          core_code_seg_sel     equ  0x38    ;内核代码段选择子
          core_data_seg_sel     equ  0x30    ;内核数据段选择子 
          sys_routine_seg_sel   equ  0x28    ;系统公共例程代码段的选择子 
@@ -13,7 +15,7 @@
 
 ;-------------------------------------------------------------------------------
          ;以下是系统核心的头部，用于加载核心程序 
-         core_length      dd core_end       ;核心程序总长度#00
+         core_length      dd core_end       ;核心程序总长度#00,记录整个内核的大小
 
          sys_routine_seg  dd section.sys_routine.start
                                             ;系统公用例程段位置#04
@@ -24,30 +26,31 @@
          core_code_seg    dd section.core_code.start
                                             ;核心代码段位置#0c
 
-
-         core_entry       dd start          ;核心代码段入口点#10
-                          dw core_code_seg_sel
+       ;核心代码的入口
+         core_entry       dd start          ;核心代码段入口点#10，偏移地址,传送到eip中
+                          dw core_code_seg_sel ;段选择子，填充的是刚刚定一个core_code_seg_sel=0x38
 
 ;===============================================================================
          [bits 32]
 ;===============================================================================
 SECTION sys_routine vstart=0                ;系统公共例程代码段 
+;内核一些api，用于提供各种用途，功能低子过程来简化代码低编写，这些既可以给内核，也可以给用户
 ;-------------------------------------------------------------------------------
          ;字符串显示例程
 put_string:                                 ;显示0终止的字符串并移动光标 
                                             ;输入：DS:EBX=串地址
          push ecx
   .getc:
-         mov cl,[ebx]
+         mov cl,[ebx];因为ds已经被我们处理好了，所以走到这里ds保持不变,
          or cl,cl
          jz .exit
-         call put_char
+         call put_char             ;调用这个来进行打印
          inc ebx
          jmp .getc
 
   .exit:
          pop ecx
-         retf                               ;段间返回
+         retf                               ;段间返回,会返回段选择子和偏移量
 
 ;-------------------------------------------------------------------------------
 put_char:                                   ;在当前光标处显示一个字符,并推进
@@ -110,7 +113,8 @@ put_char:                                   ;在当前光标处显示一个字�
          mov esi,0xa0                       ;小心！32位模式下movsb/w/d 
          mov edi,0x00                       ;使用的是esi/edi/ecx 
          mov ecx,1920
-         rep movsd
+         ;在32位下，是把数据从DS:ESI传输到ES:DSI
+         rep movsd                        ;movsd用来在两个内存区域之间进行传输双字的数据，
          mov bx,3840                        ;清除屏幕最底一行
          mov ecx,80                         ;32位程序应该使用ECX
   .cls:
@@ -229,26 +233,31 @@ put_hex_dword:                              ;在当前光标处以十六进制�
          retf
       
 ;-------------------------------------------------------------------------------
+;动态分配内存
 allocate_memory:                            ;分配内存
                                             ;输入：ECX=希望分配的字节数
                                             ;输出：ECX=起始线性地址 
          push ds
          push eax
          push ebx
-      
+         ;先让段寄存器指向数据单元
          mov eax,core_data_seg_sel
          mov ds,eax
       
-         mov eax,[ram_alloc]
+         mov eax,[ram_alloc]              ;获得ram_alloc中的值
+         ;ecx中就是西完分配的内存大小
          add eax,ecx                        ;下一次分配时的起始地址
+         ;原则上西完把eax的值写回到ram_alloc单元中，但是最好是4字节对齐
       
          ;这里应当有检测可用内存数量的指令
           
          mov ecx,[ram_alloc]                ;返回分配的起始地址
 
+       ;最好把他变成4字节对齐
          mov ebx,eax
          and ebx,0xfffffffc
-         add ebx,4                          ;强制对齐 
+         add ebx,4                          ;强制对齐 ebx的值就是4字节对齐的位置处
+         ;检查eax是否已经是4字节对齐
          test eax,0x00000003                ;下次分配的起始地址最好是4字节对齐
          cmovnz eax,ebx                     ;如果没有对齐，则强制对齐 
          mov [ram_alloc],eax                ;下次从该地址分配内存
@@ -257,7 +266,7 @@ allocate_memory:                            ;分配内存
          pop eax
          pop ds
 
-         retf
+         retf        ;只能使用远过程来调用
 
 ;-------------------------------------------------------------------------------
 set_up_gdt_descriptor:                      ;在GDT内安装一个新的描述符
@@ -272,29 +281,39 @@ set_up_gdt_descriptor:                      ;在GDT内安装一个新的描述�
       
          mov ebx,core_data_seg_sel          ;切换到核心数据段
          mov ds,ebx
-
+         ;sgdt可以将GDTR寄存器的基地址和边界数据保存到指定的内存位置
+         ;pgdt是一个6字节的内存位置
          sgdt [pgdt]                        ;以便开始处理GDT
 
+       
          mov ebx,mem_0_4_gb_seg_sel
-         mov es,ebx
+         mov es,ebx  ;
 
-         movzx ebx,word [pgdt]              ;GDT界限 
+       ;安装描述符到指定的位置
+         movzx ebx,word [pgdt]              ;GDT界限 ，我们知道GDT的界限值是16位，由于构造线性地址add需要使用两个相同的32位寄存器,前面用0填充
          inc bx                             ;GDT总字节数，也是下一个描述符偏移 
+
          add ebx,[pgdt+2]                   ;下一个描述符的线性地址 
-      
-         mov [es:ebx],eax
-         mov [es:ebx+4],edx
+         ;ebx就是下一个描述符的地址
+
+         mov [es:ebx],eax          ;把低16位安装
+         mov [es:ebx+4],edx        ;安装高16位
       
          add word [pgdt],8                  ;增加一个描述符的大小   
       
          lgdt [pgdt]                        ;对GDT的更改生效 
-       
+              
+       ;根据GDT的新界限值生成相应的段选择子
+
+
          mov ax,[pgdt]                      ;得到GDT界限值
          xor dx,dx
          mov bx,8
          div bx                             ;除以8，去掉余数
+         ;商就是我们需要的描述符号eax中
          mov cx,ax                          
-         shl cx,3                           ;将索引号移到正确位置 
+         shl cx,3                           ;将索引号移到正确位置 左移3位
+         ;TI=0指向GDT，rpl=00，0特权级
 
          pop es
          pop ds
@@ -303,7 +322,7 @@ set_up_gdt_descriptor:                      ;在GDT内安装一个新的描述�
          pop ebx
          pop eax
       
-         retf 
+         retf        ;返回
 ;-------------------------------------------------------------------------------
 make_seg_descriptor:                        ;构造存储器和系统的段描述符
                                             ;输入：EAX=线性基地址
@@ -328,18 +347,25 @@ make_seg_descriptor:                        ;构造存储器和系统的段描�
 
 ;===============================================================================
 SECTION core_data vstart=0                  ;系统核心的数据段
+;内核数据段，提供了一个可读写低内存空间，给内核自己使用 
 ;-------------------------------------------------------------------------------
          pgdt             dw  0             ;用于设置和修改GDT 
                           dd  0
 
-         ram_alloc        dd  0x00100000    ;下次分配内存时的起始地址
+         ram_alloc        dd  0x00100000    ;下次分配内存时的起始内存地址
 
+         
          ;符号地址检索表
+
          salt:
+              ;内核的salt表是静态的，所有要加载的用户程序都能使用，所以这个肯定比用户的salt大
+
+              ;262字节来表示一个条目
          salt_1           db  '@PrintString'
-                     times 256-($-salt_1) db 0
-                          dd  put_string
-                          dw  sys_routine_seg_sel
+                     times 256-($-salt_1) db 0   ;256字节的条目用来存储符号名
+                     ;6字节来填写程序的入口
+                          dd  put_string         ;4字节的偏移地址
+                          dw  sys_routine_seg_sel       ;2字节的段选择子
 
          salt_2           db  '@ReadDiskData'
                      times 256-($-salt_2) db 0
@@ -351,6 +377,8 @@ SECTION core_data vstart=0                  ;系统核心的数据段
                           dd  put_hex_dword
                           dw  sys_routine_seg_sel
 
+
+              ;用户调用这个程序，说明用户程序结束，把控制返回给内核
          salt_4           db  '@TerminateProgram'
                      times 256-($-salt_4) db 0
                           dd  return_point
@@ -377,13 +405,15 @@ SECTION core_data vstart=0                  ;系统核心的数据段
 
          esp_pointer      dd 0              ;内核用来临时保存自己的栈指针     
 
-         cpu_brnd0        db 0x0d,0x0a,'  ',0
+         cpu_brnd0        db 0x0d,0x0a,'  ',0    ;打印品牌信息，先留出空行
          cpu_brand  times 52 db 0
-         cpu_brnd1        db 0x0d,0x0a,0x0d,0x0a,0
+         cpu_brnd1        db 0x0d,0x0a,0x0d,0x0a,0      
 
 ;===============================================================================
 SECTION core_code vstart=0
+;内核代码和数据,用于分配内存，读取和加载用户程序,控制用户程序低执行
 ;-------------------------------------------------------------------------------
+;加载用户程序被定义成为了一个函数，这样就能反复调用
 load_relocate_program:                      ;加载并重定位用户程序
                                             ;输入：ESI=起始逻辑扇区号
                                             ;返回：AX=指向用户程序头部的选择子 
@@ -400,44 +430,60 @@ load_relocate_program:                      ;加载并重定位用户程序
          mov ds,eax                         ;切换DS到内核数据段
        
          mov eax,esi                        ;读取程序头部数据 
-         mov ebx,core_buf                        
+         mov ebx,core_buf                   ;数据存放的地点是我们预先开辟的内核缓存区    
          call sys_routine_seg_sel:read_hard_disk_0
-
+         ;数据填充完毕
          ;以下判断整个程序有多大
          mov eax,[core_buf]                 ;程序尺寸
          mov ebx,eax
+         ;我们发现512的倍数，低9bit都是0
          and ebx,0xfffffe00                 ;使之512字节对齐（能被512整除的数， 
-         add ebx,512                        ;低9位都为0 
+         ;先变成一个512的倍数
+         add ebx,512                        ;低9位都为0 ，再加上512字节
+         ;测试eax低9位，如果测试的结果不全为0,就使用凑的结果
          test eax,0x000001ff                ;程序的大小正好是512的倍数吗? 
+        
          cmovnz eax,ebx                     ;不是。使用凑整的结果 
       
-         mov ecx,eax                        ;实际需要申请的内存数量
+       ;接下来就是把用户程序从硬盘上读取到内存中
+         mov ecx,eax                        ;实际需要申请的内存数量,需要申请的数量就是我们读取的大小，因为我们前面读取的第一个扇区是在缓存区中
          call sys_routine_seg_sel:allocate_memory
-         mov ebx,ecx                        ;ebx -> 申请到的内存首地址
-         push ebx                           ;保存该首地址 
+         ;ecx中就是申请到的内存地址
+
+         mov ebx,ecx                        ;ebx -> 申请到的内存首地址,目的就是做为从硬盘上加载整个程序的起始地址
+         
+         push ebx                           ;保存该首地址 ,将这个地址压栈，目的是为了之后来访问用户程序的头部
+
          xor edx,edx
          mov ecx,512
          div ecx
-         mov ecx,eax                        ;总扇区数 
+         mov ecx,eax                        ;总扇区数 ,控制循环的次数
       
          mov eax,mem_0_4_gb_seg_sel         ;切换DS到0-4GB的段
          mov ds,eax
 
          mov eax,esi                        ;起始扇区号 
   .b1:
+       ;读取用户程序到内存中
          call sys_routine_seg_sel:read_hard_disk_0
          inc eax
          loop .b1                           ;循环读，直到读完整个用户程序
 
+       ;现在所有用户程序都已经读取到了内存中,就需要根据头部信息来创建段描述符
+
          ;建立程序头部段描述符
          pop edi                            ;恢复程序装载的首地址 
+         ;edi现在是该程序的起始地址
+
+         ;构造程序头部段的描述符
          mov eax,edi                        ;程序头部起始线性地址
          mov ebx,[edi+0x04]                 ;段长度
          dec ebx                            ;段界限 
          mov ecx,0x00409200                 ;字节粒度的数据段描述符
-         call sys_routine_seg_sel:make_seg_descriptor
+         call sys_routine_seg_sel:make_seg_descriptor   ;过程 返回edx:eax64位的段描述符
+         ;将描述符安装到GDT中
          call sys_routine_seg_sel:set_up_gdt_descriptor
-         mov [edi+0x04],cx                   
+         mov [edi+0x04],cx                   ;把这个段选择子写回到用户程序的头部
 
          ;建立程序代码段描述符
          mov eax,edi
@@ -447,7 +493,7 @@ load_relocate_program:                      ;加载并重定位用户程序
          mov ecx,0x00409800                 ;字节粒度的代码段描述符
          call sys_routine_seg_sel:make_seg_descriptor
          call sys_routine_seg_sel:set_up_gdt_descriptor
-         mov [edi+0x14],cx
+         mov [edi+0x14],cx         ;把这个代码段的选择子写回到内存供使用
 
          ;建立程序数据段描述符
          mov eax,edi
@@ -459,62 +505,80 @@ load_relocate_program:                      ;加载并重定位用户程序
          call sys_routine_seg_sel:set_up_gdt_descriptor
          mov [edi+0x1c],cx
 
-         ;建立程序堆栈段描述符
+         ;建立程序堆栈段描述符,堆栈所使用的空间不需要用户程序提供，而是由内核来进行动态分配
          mov ecx,[edi+0x0c]                 ;4KB的倍率 
          mov ebx,0x000fffff
          sub ebx,ecx                        ;得到段界限
          mov eax,4096                        
-         mul dword [edi+0x0c]                         
+         mul dword [edi+0x0c]               ;用4096乘倍率,得到相应堆栈大小
+
          mov ecx,eax                        ;准备为堆栈分配内存 
          call sys_routine_seg_sel:allocate_memory
          add eax,ecx                        ;得到堆栈的高端物理地址 
          mov ecx,0x00c09600                 ;4KB粒度的堆栈段描述符
          call sys_routine_seg_sel:make_seg_descriptor
          call sys_routine_seg_sel:set_up_gdt_descriptor
-         mov [edi+0x08],cx
+         mov [edi+0x08],cx                ;把构造好的堆栈段选择子写回
+
 
          ;重定位SALT
+         ;用户程序加载的时候，内核根据这些符号名来填写他们的入口地址
+         ;内核也需要创建一个SALT表
          mov eax,[edi+0x04]
-         mov es,eax                         ;es -> 用户程序头部 
+         mov es,eax                         ;es -> 用户程序头部,这个已经是我们填写好的头部选择子 
          mov eax,core_data_seg_sel
-         mov ds,eax
+         mov ds,eax                       ;ds数据段选择子
       
-         cld
+         cld         ;把direction清空，表示正向扩展
+
+       ;使用ds:esi指向CORE-SALT
+       ;使用es:edi指向User-SALT
 
          mov ecx,[es:0x24]                  ;用户程序的SALT条目数
          mov edi,0x28                       ;用户程序内的SALT位于头部内0x2c处
+        ;遍历U-salt和COre的每一项一一比较
   .b2: 
          push ecx
          push edi
       
-         mov ecx,salt_items
-         mov esi,salt
+         mov ecx,salt_items        ;将内核的条目数取出来，用来循环一一比较
+         mov esi,salt              ;要比较的内核地址
   .b3:
          push edi
          push esi
          push ecx
 
-         mov ecx,64                         ;检索表中，每条目的比较次数 
-         repe cmpsd                         ;每次比较4字节 
-         jnz .b4
-         mov eax,[esi]                      ;若匹配，esi恰好指向其后的地址数据
-         mov [es:edi-256],eax               ;将字符串改写成偏移地址 
-         mov ax,[esi+4]
-         mov [es:edi-252],ax                ;以及段选择子 
+       ;这个就是核心过程
+         mov ecx,64                         ;检索表中，每条目的比较次数 由于每个条目256字节/4=64次
+         ;
+         repe cmpsd                         ;每次比较4字节,如果两个字符串相符合，就连续比对64次
+         ;在比对结束的时候，zf=1.如果两个字符串不相等，中间就结束了
+         jnz .b4;如果zf=0,说明不相等，直接开始下一次内循环
+       
+         ;zf=1说明两个字符串相等，匹配上了
+         ;现在的任务就是把core-salt传送到u-salt条目的开始部分,头部的6字节改成了入口地址
+         
+         mov eax,[esi]                      ;若匹配，esi恰好指向其后的地址数据,eax中的4个字节就是入口地址
+         mov [es:edi-256],eax               ;将字符串改写成偏移地址 ,修改成偏移地址
+         mov ax,[esi+4]                     ;两个字节的段选择子
+         mov [es:edi-252],ax                ;以及段选择子
   .b4:
       
+
          pop ecx
          pop esi
-         add esi,salt_item_len
+         add esi,salt_item_len            ;esi加上262字节，指向下一个条目
          pop edi                            ;从头比较 
-         loop .b3
+         loop .b3                         ;内循环
       
+         
+         ;从栈顶取出来
          pop edi
-         add edi,256
+         add edi,256 ;指向下一个条目,进行比较
          pop ecx
-         loop .b2
+         loop .b2    ;外循环
 
-         mov ax,[es:0x04]
+         mov ax,[es:0x04]          ;把头部的段选择子放到ax寄存器中
 
          pop es                             ;恢复到调用此过程前的es段 
          pop ds                             ;恢复到调用此过程前的ds段
@@ -532,13 +596,20 @@ start:
          mov ecx,core_data_seg_sel           ;使ds指向核心数据段 
          mov ds,ecx
 
-         mov ebx,message_1
-         call sys_routine_seg_sel:put_string
+         mov ebx,message_1         ;因为ds指向了数据段，所以可以使用其中的标号
+         ;这个字符串是一个远转移，指令中给出了段选择子和段内偏移量
+         call sys_routine_seg_sel:put_string ;调用打印函数来显示一个字符串
                                          
          ;显示处理器品牌信息 
-         mov eax,0x80000002
+         ;处理器内部包含了大量低秘密，处理器的型号，高速缓存的数量，是否具备温度，电源管理功能
+         ;逻辑处理器的数量，高级可编程中断处理器的类型，线性地址的宽度，是否具有多媒体扩展，单指令多数据指令
+         
+         ;要返回品牌信息，使用0x800000002-0x80000004号功能，分3次进行
+         ;正常是先使用0号功能来进行
+         mov eax,0x80000002;eax中用来指定要用来返回什么样的功能
+         ;cpuid指令可以放回处理器的标识和特性信息，处理器将把信息放到eax,ebx,ecx,或者edx
          cpuid
-         mov [cpu_brand + 0x00],eax
+         mov [cpu_brand + 0x00],eax       ;执行的结果依次保存在cpu_brand开辟的52个字节的空间中
          mov [cpu_brand + 0x04],ebx
          mov [cpu_brand + 0x08],ecx
          mov [cpu_brand + 0x0c],edx
@@ -558,23 +629,29 @@ start:
          mov [cpu_brand + 0x2c],edx
 
          mov ebx,cpu_brnd0
-         call sys_routine_seg_sel:put_string
+         call sys_routine_seg_sel:put_string     ;把这个空行进行打印
          mov ebx,cpu_brand
-         call sys_routine_seg_sel:put_string
+         call sys_routine_seg_sel:put_string     ;打印品牌信息
          mov ebx,cpu_brnd1
-         call sys_routine_seg_sel:put_string
+         call sys_routine_seg_sel:put_string     ;再留空
 
+       ;开始加载用户程序
          mov ebx,message_5
          call sys_routine_seg_sel:put_string
+         
+         ;内核的主要任务就是加载和执行用户程序，这个工作是会反复进行的，一般来说这个需要定义成一个函数
          mov esi,50                          ;用户程序位于逻辑50扇区 
          call load_relocate_program
       
+         
+         ;显示信息
          mov ebx,do_status
          call sys_routine_seg_sel:put_string
       
+         ;进入用户程序后，用户程序切换到自己的堆栈,从用户程序返回的时候，还要从这个位置恢复内核堆栈指针
          mov [esp_pointer],esp               ;临时保存堆栈指针
        
-         mov ds,ax
+         mov ds,ax                        ;使段寄存器指向用户程序的头部
       
          jmp far [0x10]                      ;控制权交给用户程序（入口点）
                                              ;堆栈可能切换 
